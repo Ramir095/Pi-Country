7e0a0d1922b9b47b35013d6b2ea56ca2bfc48737
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-09-20 21:31:42 -0300

    chore: barrel file updated

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,6 @@
 export * from './Cards';
 export * from './Loading';
 export * from './Nav';
-export * from './Paginado'
+export * from './Paginado';
+export * from './Filters';
+export * from './SearchBar'
