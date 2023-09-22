636284968b8c776c903f0b1522899e86a20ecc9f
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-09-21 22:37:18 -0300

    feat: SearchBar component added in Nav

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { SearchBar } from '../SearchBar'
 import styles from './Nav.module.css'
 
 export const Nav = ({ handleClick }) => {
@@ -10,7 +9,6 @@ export const Nav = ({ handleClick }) => {
         <div className={styles.home}>
           <Link to='/home'> Home </Link>
         </div>
-        <SearchBar />
         <div className={styles.home}>
           <Link to='/createActivity'> Create Activity </Link>
           {/* <button onClick={e => handleClick(e)}>Reload</button> */}
