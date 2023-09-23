1e5c1b567e7b5f4e51a3733b9781a92a65a9bd08
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-09-22 22:22:01 -0300

    feat: added and applied customHook useForm

--- client/src/screens/CreateActivity/index.jsx
+++ client/src/screens/CreateActivity/index.jsx
@@ -1,123 +1,104 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { getAllActivities, createActivity } from "../../redux/actions/index";
-import { useDispatch, useSelector } from "react-redux";
-import { BackgroundLayout } from "../../layout";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { getAllActivities, createActivity } from '../../redux/actions/index';
+import { useDispatch, useSelector } from 'react-redux';
+import { BackgroundLayout } from '../../layout';
+import { useForm } from '../../hooks/useForm';
+import styles from './CreateCountry.module.css'
 
 export const CreateActivity = () => {
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.countries);
   const history = useHistory();
-  // const activities = useSelector(state => state.activities);
-  // console.log(activities);
-  // ¿ Dónde voy a guardar los datos que me mande el usuario ?
-  const [input, setInput] = useState({
+  const { formState, setFormState, handleChange, onResetForm } = useForm({
     name: "",
     difficulty: "",
     duration: "",
     season: "",
     country: [],
   });
-  // Quiero guardar lo que el usuario escribe en mi estado input
-  const handleChange = (e) => {
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-  };
 
   const handleSelect = (e) => {
-    setInput({
-      ...input,
-      country: [...input.country, e.target.value],
+    setFormState({
+      ...formState,
+      country: [...formState.country, e.target.value],
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(input);
-    dispatch(createActivity(input));
+    dispatch(createActivity(formState));
     alert("Creado con exito");
-    setInput({
-      name: "",
-      difficulty: "",
-      duration: "",
-      season: "",
-      country: [],
-    });
+    onResetForm();
     history.push("/home");
   };
 
-  // useEffect(() => {
-  //   dispatch(getAllActivities());
-  // }, [dispatch]);
-
   return (
     <BackgroundLayout>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input
-            id="name"
-            name="name"
-            value={input.name}
-            type="text"
-            placeholder="Mountain Bike"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="difficulty">Difficulty: </label>
-          <input
-            id="difficulty"
-            type="number"
-            value={input.difficulty}
-            name="difficulty"
-            placeholder="level"
-            min="1"
-            max="5"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="duration">Duration: </label>
-          <input
-            id="duration"
-            type="number"
-            value={input.duration}
-            name="duration"
-            placeholder="hours"
-            min="1"
-            max="10"
-            step="any"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="season">Season: </label>
-          <select
-            name="season"
-            id="season"
-            value={input.season}
-            onChange={handleChange}
-          >
-            <option value="summer">Summer</option>
-            <option value="autumn">Autumn</option>
-            <option value="winter">Winter</option>
-            <option value="spring">Spring</option>
-          </select>
-        </div>
+        <form className={ styles.formContainer } onSubmit={(e) => handleSubmit(e)}>
+          <div >
+            <label htmlFor="name">Name: </label>
+            <input
+              id="name"
+              name="name"
+              value={formState.name}
+              type="text"
+              placeholder="Mountain Bike"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="difficulty">Difficulty: </label>
+            <input
+              id="difficulty"
+              type="number"
+              value={formState.difficulty}
+              name="difficulty"
+              placeholder="level"
+              min="1"
+              max="5"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="duration">Duration: </label>
+            <input
+              id="duration"
+              type="number"
+              value={formState.duration}
+              name="duration"
+              placeholder="hours"
+              min="1"
+              max="10"
+              step="any"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="season">Season: </label>
+            <select
+              name="season"
+              id="season"
+              value={formState.season}
+              onChange={handleChange}
+            >
+              <option value="summer">Summer</option>
+              <option value="autumn">Autumn</option>
+              <option value="winter">Winter</option>
+              <option value="spring">Spring</option>
+            </select>
+          </div>
 
-        <div>
-          <select onChange={handleSelect}>
-            {countries.map((c) => (
-              <option key={c.id} value={c.name}>{c.name}</option>
-            ))}
-          </select>
-        </div>
-        <ul><li>{input.country.map(el => el + ' ,')}</li></ul>
-        <button type="submit">Create Activity</button>
-      </form>
+          <div>
+            <select onChange={handleSelect}>
+              {countries.map((c) => (
+                <option key={c.id} value={c.name}>{c.name}</option>
+              ))}
+            </select>
+          </div>
+          <ul><li>{formState.country.map(el => el + ' ,')}</li></ul>
+          <button type="submit">Create Activity</button>
+        </form>
     </BackgroundLayout>
   );
 };
