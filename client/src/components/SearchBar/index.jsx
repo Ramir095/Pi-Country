8fbbd06d5801de49da0d292885a0cdb2be3dd28c
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-09-24 21:45:13 -0300

    feat: SearchBar component created

--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCountry } from '../../redux/actions';
-import search from '../../assets/search.svg'
-import styles from './SearchBar.module.css'
+import search from '../../assets/search.svg';
+import styles from './SearchBar.module.css';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import rama from '../../assets/rama-dev.png';
 
 export const SearchBar = () => {
 
@@ -15,17 +17,21 @@ export const SearchBar = () => {
   };
 
   return (
-    <form onSubmit={handleSearch} className={ styles.form }>
-        {/* <div className={ styles.container }> */}
-          <input
-              type="text"
-              placeholder='Search country...'
-              onChange={(e) => setCountry(e.target.value)}
-              className={ styles.search }
-          />
-          <img src={ search } alt='search' className={ styles.magnifyingGlass } />
-        {/* </div> */}
-         <button className={ styles.button } type='submit'>Search</button>
-    </form>
+    <div className={ styles.searchConten }>
+      <img width={55} className={ styles.logo } src={ rama } alt='Logotipo del creador de la pagina: RamaDev' />
+      <form onSubmit={handleSearch} className={ styles.form }>
+          {/* <div className={ styles.container }> */}
+            <input
+                type="text"
+                placeholder='Search country...'
+                onChange={(e) => setCountry(e.target.value)}
+                className={ styles.search }
+            />
+            <img src={ search } alt='search' className={ styles.magnifyingGlass } />
+          {/* </div> */}
+          <button className={ styles.button } type='submit'>Search</button>
+      </form>
+      <Link className={ styles.buttonSign } to='/sign-in'>Sign in</Link>
+    </div>
   )
 };
