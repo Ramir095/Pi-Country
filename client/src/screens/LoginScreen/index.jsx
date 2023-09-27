282d6805261f958aa5ef3d86dd591c2273204708
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-09-26 22:32:11 -0300

    feat: LoginScreen created

---
+++ client/src/screens/LoginScreen/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BackgroundLayout } from '../../layout'
+
+export const LoginScreen = () => {
+  return (
+    <BackgroundLayout>
+      <div>LoginScreen</div>
+    </BackgroundLayout>
+  )
+}
