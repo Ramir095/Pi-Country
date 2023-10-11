fe76f3e5a05dfae532cba459d2fa8aa2adb2517d
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-10-10 22:11:32 -0300

    feat: LoginScreen created

--- client/src/screens/LoginScreen/index.jsx
+++ client/src/screens/LoginScreen/index.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import { BackgroundLayout } from '../../layout'
+import React from 'react';
+import { BackgroundLayout } from '../../layout';
+import { login } from '../../redux/actions';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import styles from './Login.module.css';
+import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
+  const dispatch = useDispatch();
+  const { name, handleChange } = useForm({
+    name: ''
+  });
+  const navigate = useNavigate();
+    
+  const handleLogin = () => {
+    dispatch(login(name));
+    navigate('/home', { replace: true })
+  };
   return (
     <BackgroundLayout>
-      <div>LoginScreen</div>
+      <form className={ `${styles.form} animate__animated animate__zoomIn` }>
+        <div>
+          <label className={ styles.label } htmlFor='name'>Name:</label>
+          <input 
+            name='name'
+            value={ name }
+            onChange={ handleChange }
+            className={ styles.input }
+            placeholder='user name'
+            type='text' 
+          />
+        </div>
+        <button className={ styles.buttonLogin } onClick={ handleLogin }>Login</button>
+      </form>
     </BackgroundLayout>
   )
 }
