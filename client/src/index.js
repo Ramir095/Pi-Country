1b5f4a0a530d6435480ec06890ba160dfe510f80
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-12-21 10:03:26 -0300

    feat: react-loading-skeleton and formik installed

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={ store }>
       <BrowserRouter>
-        <App />
+        <SkeletonTheme baseColor='#2d455e' highlightColor='#a39f9f'>
+          <App />
+        </SkeletonTheme>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
