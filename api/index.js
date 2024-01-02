094837362ce313ffc63cd3eeaca6d2ec5ea366ec
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2024-01-02 20:57:44 -0300

    updated

--- api/index.js
+++ api/index.js
@@ -19,11 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { PORT } = process.env;
 // Sincronizamos la tabla/ creamos la tabla
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => { // el force: true elimina la tabla y luego la vuelve a crear  
   // Cambiarlo a { alter: true } que aplica los cambios necesarios a la tabla actual para que coincida con el modelo
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
   });
 });
