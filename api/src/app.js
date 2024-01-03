598a9a15d236772ab0c765b3133f7b29ff82456d
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2024-01-02 22:16:07 -0300

    feat: route changed

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const routesCountries = require('./routes/routesCountries.js');
 const routesActivities = require('./routes/routesActivities')
 require('./db.js');
+const {
+  URL
+} = process.env;
 
 const server = express();
 
@@ -15,7 +18,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://pi-country-iqpo.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', `${URL}`); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
